f7d34fbe8984480fb955e850941240bd730f5110
Lorenzo <lorenzopiatti2003@gmail.com>
2023-09-06 19:08:06 -0300

    first commit

--- Visual Studio Code/C/LC1-U3-Practica1/U3E10.c
+++ Visual Studio Code/C/LC1-U3-Practica1/U3E10.c
@@ -13,52 +13,47 @@ void main()
     printf("Ingrese un numero del 1 al 12 y te dire a que mes corresponde:\n");
     scanf("%i", &numeroIngresado);
 
-    if (numeroIngresado < 12 || 1 < numeroIngresado)
+    switch (numeroIngresado)
     {
-        switch (numeroIngresado)
-        {
-        case 1:
-            printf("El numero ingresado corresponde al mes Enero\n");
-            break;
-        case 2:
-            printf("El numero ingresado corresponde al mes Febrero\n");
-            break;
-        case 3:
-            printf("El numero ingresado corresponde al mes Marzo\n");
-            break;
-        case 4:
-            printf("El numero ingresado corresponde al mes Abril\n");
-            break;
-        case 5:
-            printf("El numero ingresado corresponde al mes Mayo\n");
-            break;
-        case 6:
-            printf("El numero ingresado corresponde al mes Junio\n");
-            break;
-        case 7:
-            printf("El numero ingresado corresponde al mes Julio\n");
-            break;
-        case 8:
-            printf("El numero ingresado corresponde al mes Agosto\n");
-            break;
-        case 9:
-            printf("El numero ingresado corresponde al mes Septiembre\n");
-            break;
-        case 10:
-            printf("El numero ingresado corresponde al mes Octubre\n");
-            break;
-        case 11:
-            printf("El numero ingresado corresponde al mes Noviembre\n");
-            break;
-        case 12:
-            printf("El numero ingresado corresponde al mes Diciembre\n");
-            break;
-        }
-    }
-    if (numeroIngresado > 12 || numeroIngresado < 1)
-    {
+    case 1:
+        printf("El numero ingresado corresponde al mes Enero\n");
+        break;
+    case 2:
+        printf("El numero ingresado corresponde al mes Febrero\n");
+        break;
+    case 3:
+        printf("El numero ingresado corresponde al mes Marzo\n");
+        break;
+    case 4:
+        printf("El numero ingresado corresponde al mes Abril\n");
+        break;
+    case 5:
+        printf("El numero ingresado corresponde al mes Mayo\n");
+        break;
+    case 6:
+        printf("El numero ingresado corresponde al mes Junio\n");
+        break;
+    case 7:
+        printf("El numero ingresado corresponde al mes Julio\n");
+        break;
+    case 8:
+        printf("El numero ingresado corresponde al mes Agosto\n");
+        break;
+    case 9:
+        printf("El numero ingresado corresponde al mes Septiembre\n");
+        break;
+    case 10:
+        printf("El numero ingresado corresponde al mes Octubre\n");
+        break;
+    case 11:
+        printf("El numero ingresado corresponde al mes Noviembre\n");
+        break;
+    case 12:
+        printf("El numero ingresado corresponde al mes Diciembre\n");
+        break;
+    default:
         printf("Error.. Numero ingresado es mayor a 12 o menor a 1.\n");
+        break;
     }
-    system("pause");
-    
+system("pause");
 }
