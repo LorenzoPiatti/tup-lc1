2a11b00e906058809a362f66da5e95c79f20c2e8
Lorenzo <lorenzopiatti2003@gmail.com>
2023-09-06 19:07:56 -0300

    first commit

---
+++ Visual Studio Code/C/LC1-U3-Practica1/U3E11.c
@@ -0,0 +1,18 @@
+/*11- Pedir al usuario que ingrese dos números (permitir ingresar número con decimales).
+Luego presentar el siguiente menú:
+1. Informar su suma
+2. Informar su resta
+3. Informar su multiplicación
+4. Informar su división
+5. Salir
+Seleccione una operación:
+Mostrar el resultado de la operación seleccionada. Si el usuario ingresa la opción 5 el
+programa debe terminar.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main()
+{
+    int 
+}
